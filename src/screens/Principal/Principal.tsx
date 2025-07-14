a07b63184334074fe4643e2b7e7ce7bb47f31692
Henrique Sogayar <henriquesogayar@gmail.com>
2025-07-14 16:45:27 -0300

    modified:   src/screens/Principal/Principal.tsx

--- src/screens/Principal/Principal.tsx
+++ src/screens/Principal/Principal.tsx
@@ -16,7 +16,7 @@ export const Principal = (): JSX.Element => {
       <HeaderSection />
       <main className="w-full bg-gray-50">
         <HeroSection />
-        <StatisticsSection />
+        {/* <StatisticsSection /> */}
         <StatisticsAndAboutUsSection />
         <OurServices />
         <TestimonialsSection />
